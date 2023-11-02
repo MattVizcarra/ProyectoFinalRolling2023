6c0ae9195c0bf583ed6da242d074bb6da9a2c3c2
MattVizcarra <matias.vizcarra.mv@gmail.com>
2023-11-02 20:33:00 -0300

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+function App() {
+
+
+
+  return (
+    <>
+    <BrowserRouter>
+    {/* <Navbar></Navbar> */}
+    <h1>Hello World</h1>
+    <button className='btn btn-primary'>Boton</button>
+    <main>
+      <Routes>
+        {/* <Route exact path="/" element={<Inicio/>} /> */}
+        {/* <Route path="/cart" element={<SobreNosotros/>}></Route> */}
+      </Routes>
+    </main>
+    {/* <Footer></Footer> */}
+
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
