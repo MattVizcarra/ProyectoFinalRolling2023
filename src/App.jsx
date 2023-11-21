9ace3b668b0afa61bc25da378b470d98de7896bf
MattVizcarra <matias.vizcarra.mv@gmail.com>
2023-11-21 19:42:42 -0300

    Limpiando el codigo de la rama develope

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import PaginaPrincipal from './Components/paginaPrincipal/paginaPrincipal'
 import Navbar from './Components/Navbar/Navbar'
 import Footer from './Components/Footer/Footer'
 
-
-import PaginaPrincipal from './components/paginaPrincipal/paginaPrincipal'
 
 function App() {
 
@@ -14,17 +13,14 @@ function App() {
   return (
     <>
     <BrowserRouter>
-    {/* <Navbar></Navbar> */}
-    <h1>Hello World</h1>
-    <h2>Desde rama develope</h2>
-    <Navbar></Navbar>
-    <button className='btn btn-primary'>Boton</button>
     <main>
+      <Navbar></Navbar>
       <Routes>
         <Route exact path="/" element={<PaginaPrincipal/>} />
         {/* <Route path="/cart" element={<SobreNosotros/>}></Route> */}
       </Routes>
     </main>
+    <Footer></Footer>
 
       </BrowserRouter>
     </>
