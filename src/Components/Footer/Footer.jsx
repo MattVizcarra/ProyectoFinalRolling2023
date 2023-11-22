e552cd204d099a6bf6b7e579b420c09537721cf9
MattVizcarra <matias.vizcarra.mv@gmail.com>
2023-11-22 14:33:47 -0300

    Se agrego el logo de la pagina

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,29 +2,35 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import '../Footer/Footer.css';
+import { Route, Routes } from 'react-router-dom';
+import { Link } from "react-router-dom";
+
 
 
 function Footer() {
+
+const imageUrl = 'https://th.bing.com/th/id/R.fbd95cf106ab3bcd3acb48263703790d?rik=xbEXpTHKreYWQQ&pid=ImgRaw&r=0'
 
   return (
 <>
 <div className='Footer'>
     <Container>
       <Row className='Footer1 d-flex-container justify-content-center  y-md-5'>
-        <Col className='col1-Footer  col-xl-3 col-lg-10 col-md-10 col-sm-12'>
+        <Col className='col1-Footer  col-xl-3 col-lg-10 col-md-10 col-sm-10'>
             <div className="container text-center">
+            <Link to='/'><img src={imageUrl} className='ImgFoter' alt="Logo de la pagina" /></Link>
             <h3 className="LetraFooter">ABRIMOS:</h3>
             <p className="LetraFooter">Lunes - Sabado</p>
             <p className="LetraFooter">12 - 21 hs.</p></div>
-            </Col>
-        <Col className='col1-Footer  col-xl-3 col-lg-10 col-md-10 col-sm-12'>
+        </Col>
+        <Col className='col1-Footer  col-xl-3 col-lg-10 col-md-10 col-sm-10'>
             <div className="container text-center">
             <h3 className="LetraFooterTitulo">¿Tienes Hambre?</h3><br />
             <p className="LetraFooter">Te invitamos a hacer tu pedido</p>
             <p className="LetraFooter">Av. Buenos Aires 534</p>
             <p className="LetraFooter">Tucumán, Argentina</p></div>
         </Col>
-        <Col className='col1-Footer  col-xl-3 col-lg-10 col-md-10 col-sm-12'>
+        <Col className='col1-Footer  col-xl-3 col-lg-10 col-md-10 col-sm-10'>
             <div className="container text-center">
             <p className="LetraFooter">Tel: 3817697678</p>
             <p className="LetraFooter">Email:dscsk@gmail.com</p></div>
@@ -32,6 +38,9 @@ function Footer() {
         
       </Row>
     </Container>
+    <Routes>
+        <Route path='/'/>
+    </Routes>
 </div>
 </>
   );
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import "../Navbar/Navbar.css";
 import Tarjetas from "../Tarjetas/Tarjetas";
+import Login from "../Acceso/Acceso";
+import Nosotros from "../Nosotros/Nosotros";
 import { Route, Routes } from 'react-router-dom';
 
 import { Link } from "react-router-dom";
-import Login from "../Acceso/Acceso";
+
 
 function Navbar() {
+  const imageUrl = 'https://th.bing.com/th/id/R.fbd95cf106ab3bcd3acb48263703790d?rik=xbEXpTHKreYWQQ&pid=ImgRaw&r=0'
 return (
 <>
 <header>
 <nav className="Navbar">
   <div>
     <ul className="PaginasNav">
+    <Link to='/'><img src={imageUrl} className='ImgNavbar' alt="Logo de la pagina" /></Link>
       <Link to='/'><li>INICIO</li></Link>
-      <Link to='/'><li>CONTACTO</li></Link>
+      <Link to='/Nosotros'><li>NOSOTROS</li></Link>
       <Link to='/Tarjetas'><li>MENÚ</li></Link>
     </ul>
   </div>
@@ -25,6 +29,7 @@ return (
       <Route path='/'/>
       <Route path='/Tarjetas' Component={Tarjetas}/> 
       <Route path='/Acceso' Component={Login} />
+      <Route path='/Nosotros' Component={Nosotros}/>
     </Routes>
 
 
